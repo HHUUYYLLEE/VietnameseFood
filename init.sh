9c3cb5d234f863f8adbd6da0b4b34d7601796ae0
tuyenldhust <tuyen.ld194715@sis.hust.edu.vn>
2023-06-01 11:58:49 +0700

    edit init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-docker compose up --build -d
-docker exec app-food composer install --ignore-platform-reqs
-docker exec app-food php artisan key:generate
-docker exec app-food php artisan optimize
-docker exec app-food php artisan migrate:refresh
-docker exec app-food php artisan db:seed -force
+docker-compose up --build -d
+docker-compose exec app-food composer install --ignore-platform-reqs
+docker-compose exec app-food php artisan key:generate
+docker-compose exec app-food php artisan optimize
+docker-compose exec app-food php artisan migrate:refresh
+docker-compose exec app-food php artisan db:seed --force
