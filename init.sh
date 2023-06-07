0850d3892245ef12e2b06ee93e52a5e8273d31d2
tuyenldhust <tuyen.ld194715@sis.hust.edu.vn>
2023-06-07 21:32:47 +0700

    fix bash script

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 chmod -R 777 ./
+cp .env.example .env
 docker-compose up --build -d
 docker-compose exec app composer install --ignore-platform-reqs
 docker-compose exec app php artisan key:generate
